247e197c93591dfc0ef9da38066905fb7088145c
Luis Fernando Quintero Niño <112994319+plquinte2@users.noreply.github.com>
2022-09-22 09:41:35 -0500

    22092022

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/login')
 def login():
     return render_template('login.html')
